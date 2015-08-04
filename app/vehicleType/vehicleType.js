f8a651e789ebbba9abb738bc15fb543d639b6bcc
dihmuzikien <nilparus@por-nilparus-m.ds.ad.adp.com>
2015-08-04 15:20:55 -0700

    vehicleType update

--- app/vehicleType/vehicleType.js
+++ app/vehicleType/vehicleType.js
@@ -1,14 +1,37 @@
 'use strict';
 
-angular.module('myApp.vehicleType', [])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/vehicleType', {
-    templateUrl: 'vehicleType/vehicleType.html',
-    controller: 'VehicleTypeCtrl'
-  });
-}])
+(function(){ 
+	var module = angular.module('myApp.vehicleType', [])
 
-.controller('VehicleTypeCtrl', [function() {
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/vehicleType', {
+			templateUrl: 'vehicleType/vehicleType.html',
+			controller: 'VehicleTypeCtrl'
+		});
+	}]);
 
-}]);
+	module.controller('VehicleTypeCtrl', [function() {
+
+
+	}]);
+
+	module.controller('sedanController',["$scope",function($scope){
+		$scope.myInterval = 1000;
+		$scope.noWrapSlides = false;
+		var slides = $scope.slides = [];
+		$scope.addSlide = function() {
+			var newWidth = 600 + slides.length + 1;
+			slides.push({
+				image: '//placekitten.com/' + newWidth + '/300',
+				text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+				['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+			});
+		};
+		for (var i=0; i<4; i++) {
+			$scope.addSlide();
+		}
+
+	}]);
+
+}());
--- app/vehicleType/vehicleType_test.js
+++ app/vehicleType/vehicleType_test.js
@@ -2,15 +2,41 @@
 
 describe('myApp.vehicleType module', function() {
 
-  beforeEach(module('myApp.vehicleType'));
+	beforeEach(module('myApp.vehicleType'));
 
-  describe('vehicleType controller', function(){
+	describe('vehicleType controller', function(){
 
-    it('should ....', inject(function($controller) {
+		it('should ....', inject(function($controller) {
       //spec body
       var vehicleTypeCtrl = $controller('VehicleTypeCtrl');
       expect(vehicleTypeCtrl).toBeDefined();
-    }));
+  }));
 
-  });
+	});
 });
+
+(function(){
+
+	var module = angular.module('vehicleTypeView', []);
+
+	var slideController = function($scope){
+		$scope.myInterval = 5000;
+		$scope.noWrapSlides = false;
+		var slides = $scope.slides = [];
+		$scope.addSlide = function() {
+			var newWidth = 600 + slides.length + 1;
+			slides.push({
+				image: '//placekitten.com/' + newWidth + '/300',
+				text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+				['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+			});
+		};
+		for (var i=0; i<4; i++) {
+			$scope.addSlide();
+		}
+	};
+
+	module.controller("slideController", slideController);
+
+
+}());
