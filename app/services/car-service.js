9521aaa48574d7136b20c04e134b8980da67abb0
Timothy Jensen <timothyjamesjensen@gmail.com>
2015-08-05 16:37:19 -0700

    ll

--- app/services/car-service.js
+++ app/services/car-service.js
@@ -3,7 +3,7 @@
 angular.module('myApp.services', [])
     .service('carService', function() {
         return {
-                car: 'coupes',
+                car: '',
             getCars: function() {
                 return {
                 "types": [
