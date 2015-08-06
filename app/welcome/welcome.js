8b5c13a1dbd61ef3d10bd1f63aaee0ef36eba094
jblackm2 <jblackm2@pdx.edu>
2015-08-06 15:45:50 -0700

    Trying to get the vehicleModel to display a 2x2 grid of images

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -2,6 +2,7 @@
 
 angular.module('myApp.welcome', ['myApp.services'])
 
+
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/welcome', {
     templateUrl: 'welcome/welcome.html',
@@ -10,8 +11,28 @@ angular.module('myApp.welcome', ['myApp.services'])
 }])
 
 .controller('WelcomeCtrl', ['$scope', '$location', function($scope, $location) {
+
     $scope.go = function ( path ) {
     	//localStorage.setItem("lastname", "Smith");
         $location.path( path );
+
+
     };
+
+    $scope.audio = function() {
+        var flag = new Boolean(false);
+        var audio = document.getElementById("audio");
+        audio.load();
+        audio.play();
+        if(audio.addEventListener("ended", null)){
+            flag = true;
+        }
+
+        if(flag == true){
+            $scope.go('/vehicleType');
+        }
+
+    };
+
 }]);
+
